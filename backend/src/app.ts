ebfa866c261bf0392b0f0ba8f1c9e0769b06476a
Vinhn1 <vinhnguyen04275@gmail.com>
2026-04-28 10:53:06 +0000

    feat: integrate global error handler and test operational errors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from 'express';
+import AppError from './utils/appError';
 import dotenv from 'dotenv'; 
 import cors from 'cors';
 import helmet from 'helmet';
+import globalErrorHandler from './middlewares/errorMiddleware';
 
 // Khởi tạo cấu hình biến môi trường 
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -23,6 +24,14 @@ app.get('/', (req: Request, res: Response) => {
         message: 'NexTask API is running with TypeScript!'
     });
 });
+
+app.get('/test-error', (req, res, next) => {
+    // Ném lỗi vào vòng tay của Error Middleware bằng cách gọi next(err)
+    next(new AppError('Đang có lỗi xảy ra!', 400));
+});
+
+// Gắn Error Middleware 
+app.use(globalErrorHandler);
 
 // Lắng nghe cổng
 app.listen(PORT, () => {
---
+++ backend/src/middlewares/errorMiddleware.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from 'express';
+import AppError from '../utils/appError';
+
+const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    //  Lấy statusCode và status từ err, nếu không có thì mặc định (500 và 'error')
+    const statusCode = err.statusCode || 500;
+    const status = err.status || 'error';
+
+    // Xác định môi trường hiện tại
+    const isDevelopment = process.env.NODE_ENV === 'development';
+
+    if(isDevelopment){
+        // Môi trường phát triển: trả về đầy đủ thông tin để debug
+        res.status(statusCode).json({
+            status,
+            message: err.message,
+            stack: err.stack,
+            error: err, // Gửi toàn bộ obj lỗi để đễ xem chi tiết 
+        });
+    }else{
+        // Môi trường production
+        // Ghi log chi tiết lỗi (thay thể console.error bằng Wintons sau )
+        console.error('ERROR: ', err);
+
+        if(err.isOperational){
+            // lỗi vận hành (do ta chủ động tạo bằng AppError): an toàn để gửi cho client 
+            res.status(statusCode).json({
+                status,
+                message: err.message,
+            });
+        }else{
+            // Lỗi lập trình hoặc lỗi không xác định: ẩn chi tiết khỏi client
+            res.status(statusCode).json({
+                status: 'error',
+                message: 'Something went wrong!'
+            });
+        }
+    }
+};
+
+export default globalErrorHandler;
