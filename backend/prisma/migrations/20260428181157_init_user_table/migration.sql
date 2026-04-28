f5342bece124768c8886753a62721540e8a03186
Vinhn1 <vinhnguyen04275@gmail.com>
2026-04-28 18:18:06 +0000

    feat: implement partial unique index for email with soft delete

---
+++ backend/prisma/migrations/20260428181157_init_user_table/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'MEMBER');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "fullname" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'MEMBER',
+    "email_verified_at" TIMESTAMP(3),
+    "refresh_token_version" INTEGER NOT NULL DEFAULT 1,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email") WHERE "deleted_at" IS NULL;
