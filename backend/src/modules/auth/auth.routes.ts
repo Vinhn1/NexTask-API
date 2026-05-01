e1f203929a9d4a5e637b2b2156c6a6fcc9e252ab
Vinhn1 <vinhnguyen04275@gmail.com>
2026-05-01 14:46:07 +0000

    feat: complete validation and authorization phase

--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -1,17 +1,24 @@
 import { Router } from 'express';
 import { authController } from './auth.controller';
 import { protect } from '../../middlewares/authMiddleware';
+import { validate } from '../../middlewares/validate';
+import { registerSchema, loginSchema } from './auth.dto';
+import { restrictTo } from '../../middlewares/restrictTo';
 
 const router = Router();
 
 // Định nghĩa tuyến đường POST /register
-router.post('/register', authController.register);
+router.post('/register', validate(registerSchema), authController.register);
 
 // Định nghĩa tuyến đường POST /login
-router.post('/login', authController.login);
+router.post('/login', validate(loginSchema), authController.login);
 
 router.get('/me', protect, authController.getMe);
 
 router.post('/logout', protect, authController.logout);
+
+router.get('/all-users', protect, restrictTo('ADMIN'), (req, res) => res.json({
+    message: 'Chào Sếp!'
+}));
 
 export default router;
