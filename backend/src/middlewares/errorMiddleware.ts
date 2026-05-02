dc1888e3d72fb764f91a1f520bf2870d96cfcbeb
Vinhn1 <vinhnguyen04275@gmail.com>
2026-05-02 13:42:09 +0000

    feat(api): implement standardized ApiResponse and Global Error Middleware

--- backend/src/middlewares/errorMiddleware.ts
+++ backend/src/middlewares/errorMiddleware.ts
@@ -1,39 +1,41 @@
 import { Request, Response, NextFunction } from 'express';
+import  { ApiResponse } from '../utils/apiResponse';
+import { AppError } from '../utils/appError';
 
 const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     //  Lấy statusCode và status từ err, nếu không có thì mặc định (500 và 'error')
-    const statusCode = err.statusCode || 500;
-    const status = err.status || 'error';
+    err.statusCode = err.statusCode || 500;
 
-    // Xác định môi trường hiện tại
-    const isDevelopment = process.env.NODE_ENV === 'development';
-
-    if(isDevelopment){
-        // Môi trường phát triển: trả về đầy đủ thông tin để debug
-        res.status(statusCode).json({
-            status,
-            message: err.message,
-            stack: err.stack,
-            error: err, // Gửi toàn bộ obj lỗi để đễ xem chi tiết 
-        });
+    // DEVELOPMENT MODE
+    // Hiển thị toàn bộ lỗi để debug
+    if(process.env.NODE_ENV === 'development'){
+        console.error('DEV ERROR ', err);
+        return ApiResponse.error(
+            res,
+            err.message,
+            err.statusCode,
+            err.stack
+        );
     }else{
-        // Môi trường production
-        // Ghi log chi tiết lỗi (thay thể console.error bằng Wintons sau )
-        console.error('ERROR: ', err);
-
+        // PRODUCTION MODE
+        // Không được leak thông tin hệ thống
+        // Lỗi do ta chủ động xử lý 
         if(err.isOperational){
-            // lỗi vận hành (do ta chủ động tạo bằng AppError): an toàn để gửi cho client 
-            res.status(statusCode).json({
-                status,
-                message: err.message,
-            });
-        }else{
-            // Lỗi lập trình hoặc lỗi không xác định: ẩn chi tiết khỏi client
-            res.status(statusCode).json({
-                status: 'error',
-                message: 'Something went wrong!'
-            });
+            return ApiResponse.error(
+                res,
+                err.message,
+                err.statusCode
+            );
         }
+
+        // Lỗi hệ thống không xác định 
+        console.error('PRODUCTION ERROR: ', err);
+
+        return ApiResponse.error(
+            res,
+            'Something went very wrong!',
+            500
+        ); 
     }
 };
 
