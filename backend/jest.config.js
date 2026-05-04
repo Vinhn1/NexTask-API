9f9758e1413700857b2fc56d6c86a042141073c8
Vinhn1 <vinhnguyen04275@gmail.com>
2026-05-04 16:46:15 +0000

    feat(testing): complete auth service unit tests with 81% coverage

---
+++ backend/jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/**/*.test.ts'], // Chỉ chạy các file có đuôi .test.ts
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1', // Support alias nếu bạn có dùng
+  },
+};
